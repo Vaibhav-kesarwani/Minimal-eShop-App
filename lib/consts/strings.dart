6b91135ba6c2364af12f7a267ae9dd377303e02f
Vaibhav Kesarwani <vaibhavkesarwani100@gmail.com>
2023-12-16 20:33:32 +0530

    Added basic card items

--- lib/consts/strings.dart
+++ lib/consts/strings.dart
@@ -1,3 +1,12 @@
 const appname = "eShop";
 const appversion = "Version 1.0.0";
 const credits = "@vaibhav_kesarwani";
+const email = "Email";
+const emailHint = "admin@admin.dev";
+const password = "Password";
+const passwordHint = "********";
+const forgetpass = "Forget Password";
+const login = "Login";
+const signup = "Signup";
+const createNewAccount = "or, create a new account";
+const loginWith = "Login with";
--- lib/views/auth_screen/login_screen.dart
+++ lib/views/auth_screen/login_screen.dart
@@ -1,6 +1,8 @@
 import 'package:ecommerceapp/consts/consts.dart';
 import 'package:ecommerceapp/widgets_common/applogo_widget.dart';
 import 'package:ecommerceapp/widgets_common/bg_widget.dart';
+import 'package:ecommerceapp/widgets_common/custom_textfield.dart';
+import 'package:ecommerceapp/widgets_common/our_button.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -18,6 +20,43 @@ class LoginScreen extends StatelessWidget {
               10.heightBox,
               "Log in to $appname".text.fontFamily(bold).white.size(18).make(),
               10.heightBox,
+              Column(
+                children: [
+                  customTextField(hint: emailHint, title: email),
+                  customTextField(hint: passwordHint, title: password),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: TextButton(
+                        onPressed: () {}, child: forgetpass.text.make()),
+                  ),
+                  5.heightBox,
+                  ourButton(
+                    color: redColor,
+                    title: login,
+                    textColor: whiteColor,
+                    onPress: () {},
+                  ).box.width(context.screenWidth - 50).make(),
+                  10.heightBox,
+                  createNewAccount.text.color(fontGrey).make(),
+                  10.heightBox,
+                  ourButton(
+                    color: lightGolden,
+                    title: signup,
+                    textColor: redColor,
+                    onPress: () {},
+                  ).box.width(context.screenWidth - 50).make(),
+
+                  10.heightBox,
+                  loginWith.text.color(fontGrey).make(),
+                  5.heightBox,
+                ],
+              )
+                  .box
+                  .white
+                  .rounded
+                  .padding(const EdgeInsets.all(16))
+                  .width(context.screenWidth - 70)
+                  .make(),
             ],
           ),
         ),
